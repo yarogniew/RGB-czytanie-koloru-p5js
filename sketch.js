acc3b18fb7c46de2214e9d8879013a19c8c9fb44
yarogniew <1@yarogniew.net>
2018-03-06 19:23:17 +0100

    wersja z liczbami działająca

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 /* P5JS CODE */
 var img;
 var c;
+var d;
 //var rgbValue;
 
 function preload()
@@ -11,6 +12,7 @@ function preload()
 function setup() {
     createCanvas(380, 480);
     c = color('white');
+    d = -50;
 }
 
 function draw() {
@@ -21,23 +23,25 @@ function draw() {
     //stroke(255);
     fill(c);
     rect(width/4, 2, width/2, 20);
-    //fill(255);
-    //text(rgbValue, 10, 10);
+    var rgbV = c[0] + "," + c[1] + "," + c[2];
+    fill(c[0]+d, c[1]+d, c[2]+d);
+    textAlign(CENTER);
+    if (c[0]<255)
+      {
+        text(rgbV , width/2, 32);
+      }
 }
 
 function mousePressed()
 {
     c = get(mouseX, mouseY);
 
-    var rgbValue = c[0] + "," + c[1] + "," + c[2];
-    print(rgbValue);
-    //fill(255);
-    //text(rgbValue, 10, 10, 70, 80);
-    var particle = new Particle();
-    particle.callFunction({
-       deviceId: 'YOURDEVICEID',
-       name: 'led',
-       argument: rgbValue,
-       auth: 'YOURAUTHVALUE'
-    });
+
+    // var particle = new Particle();
+    //particle.callFunction({
+       //deviceId: 'YOURDEVICEID',
+       //name: 'led',
+       //argument: rgbValue,
+       //auth: 'YOURAUTHVALUE'
+    //});
 }
